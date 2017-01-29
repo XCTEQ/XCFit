c77c1d3ee57fb7a3d92f9fd270b3f6ccea374964
Shashikant86 <shashikant.jagtap@icloud.com>
2017-01-29 23:07:37 +0000

    Adding Carthage support for project

---
+++ XCFitFramework/XCFitFramework.h
@@ -0,0 +1,19 @@
+//
+//  XCFitFramework.h
+//  XCFitFramework
+//
+//  Created by Shashikant Jagtap on 29/01/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XCFitFramework.
+FOUNDATION_EXPORT double XCFitFrameworkVersionNumber;
+
+//! Project version string for XCFitFramework.
+FOUNDATION_EXPORT const unsigned char XCFitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCFitFramework/PublicHeader.h>
+
+
