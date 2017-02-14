1852359c65b8dbe53cb45a78aaf975a575265b4e
Shashikant86 <shashikant.jagtap@icloud.com>
2017-02-14 19:28:40 +0000

    Swift 3.0.2 support

--- Sources/XCFit.swift
+++ Sources/XCFit.swift
@@ -13,43 +13,43 @@ open class XCFit: XCTestCase {
     }
 
 
-    open func waitForElementToAppearCommpleted(_ element: XCUIElement) -> Bool {
-        let predicate = NSPredicate(format: "exists == true AND hittable == true AND enabled == true")
-        let xcfitExpectation = expectation(for: predicate, evaluatedWith: element,
-                                           handler: nil)
-
-        let result = XCTWaiter().wait(for: [xcfitExpectation], timeout: 60)
-        return result == .completed
-
-    }
-
-    open func waitForElementToAppearTimedOut(_ element: XCUIElement) -> Bool {
-        let predicate = NSPredicate(format: "exists == true AND hittable == true AND enabled == true")
-        let xcfitExpectation = expectation(for: predicate, evaluatedWith: element,
-                                           handler: nil)
-        let result = XCTWaiter().wait(for: [xcfitExpectation], timeout: 60)
-        return result == .timedOut
-    }
-
-    open func waitForElementToAppearIncorrectOrder(_ element: XCUIElement) -> Bool {
-        let predicate = NSPredicate(format: "exists == true AND hittable == true AND enabled == true")
-        let xcfitExpectation = expectation(for: predicate, evaluatedWith: element,
-                                           handler: nil)
-        let result = XCTWaiter().wait(for: [xcfitExpectation], timeout: 60)
-        return result == .incorrectOrder
-    }
-
-    open func waitForElementToAppearinvertedFulfillment(_ element: XCUIElement) -> Bool {
-        let predicate = NSPredicate(format: "exists == true AND hittable == true AND enabled == true")
-        let xcfitExpectation = expectation(for: predicate, evaluatedWith: element,
-                                           handler: nil)
-        let result = XCTWaiter().wait(for: [xcfitExpectation], timeout: 60)
-        return result == .invertedFulfillment
-    }
-
+//    open func waitForElementToAppearCommpleted(_ element: XCUIElement) -> Bool {
+//        let predicate = NSPredicate(format: "exists == true AND hittable == true AND enabled == true")
+//        let xcfitExpectation = expectation(for: predicate, evaluatedWith: element,
+//                                           handler: nil)
+//
+//        let result = XCTWaiter().wait(for: [xcfitExpectation], timeout: 60)
+//        return result == .completed
+//
+//    }
+//
+//    open func waitForElementToAppearTimedOut(_ element: XCUIElement) -> Bool {
+//        let predicate = NSPredicate(format: "exists == true AND hittable == true AND enabled == true")
+//        let xcfitExpectation = expectation(for: predicate, evaluatedWith: element,
+//                                           handler: nil)
+//        let result = XCTWaiter().wait(for: [xcfitExpectation], timeout: 60)
+//        return result == .timedOut
+//    }
+//
+//    open func waitForElementToAppearIncorrectOrder(_ element: XCUIElement) -> Bool {
+//        let predicate = NSPredicate(format: "exists == true AND hittable == true AND enabled == true")
+//        let xcfitExpectation = expectation(for: predicate, evaluatedWith: element,
+//                                           handler: nil)
+//        let result = XCTWaiter().wait(for: [xcfitExpectation], timeout: 60)
+//        return result == .incorrectOrder
+//    }
+//
+//    open func waitForElementToAppearinvertedFulfillment(_ element: XCUIElement) -> Bool {
+//        let predicate = NSPredicate(format: "exists == true AND hittable == true AND enabled == true")
+//        let xcfitExpectation = expectation(for: predicate, evaluatedWith: element,
+//                                           handler: nil)
+//        let result = XCTWaiter().wait(for: [xcfitExpectation], timeout: 60)
+//        return result == .invertedFulfillment
+//    }
+//
     open func elementAppeared(_ element: XCUIElement) {
-        let result = waitForElementToAppearCommpleted(element)
-        XCTAssertTrue(result)
-//        waitUntilElementActive(element: element)
+//        let result = waitForElementToAppearCommpleted(element)
+//        XCTAssertTrue(result)
+        waitUntilElementActive(element: element)
     }
 }
--- Sources/XCFitWaiter.swift
+++ Sources/XCFitWaiter.swift
@@ -12,25 +12,25 @@ import Foundation
 @available(OSX 10.11, *)
 extension XCFit {
     
-    open func whenIexpectElementToAppear(_ element: XCUIElement) {
-        let result = waitForElementToAppearCommpleted(element)
-        XCTAssertTrue(result)
-    }
-    
-    open func whenIexpectElementTimeOut(_ element: XCUIElement) {
-        let result = waitForElementToAppearTimedOut(element)
-        XCTAssertTrue(result)
-    }
-    
-    open func whenIexpectElementIncorrectOrder(_ element: XCUIElement) {
-        let result = waitForElementToAppearIncorrectOrder(element)
-        XCTAssertTrue(result)
-    }
-    
-    open func whenIexpectElementInvertedFulfillment(_ element: XCUIElement) {
-        let result = waitForElementToAppearinvertedFulfillment(element)
-        XCTAssertTrue(result)
-    }
+//    open func whenIexpectElementToAppear(_ element: XCUIElement) {
+//        let result = waitForElementToAppearCommpleted(element)
+//        XCTAssertTrue(result)
+//    }
+//    
+//    open func whenIexpectElementTimeOut(_ element: XCUIElement) {
+//        let result = waitForElementToAppearTimedOut(element)
+//        XCTAssertTrue(result)
+//    }
+//    
+//    open func whenIexpectElementIncorrectOrder(_ element: XCUIElement) {
+//        let result = waitForElementToAppearIncorrectOrder(element)
+//        XCTAssertTrue(result)
+//    }
+//    
+//    open func whenIexpectElementInvertedFulfillment(_ element: XCUIElement) {
+//        let result = waitForElementToAppearinvertedFulfillment(element)
+//        XCTAssertTrue(result)
+//    }
 }
 
 
