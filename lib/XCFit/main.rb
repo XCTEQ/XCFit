0ed42b29cfc23fb8b3df16a266c90d9a4ed2859d
Shashikant86 <shashikant.jagtap@icloud.com>
2017-04-10 20:20:33 +0100

    Fastfile Setup

--- lib/XCFit/main.rb
+++ lib/XCFit/main.rb
@@ -7,6 +7,8 @@ module XCFit
 	  include Thor::Actions
     $source_dir = File.expand_path '../../..', __FILE__
 		$source_cocoapods_dir = File.join($source_dir, 'Cocoapods')
+		$source_fastlane_dir = File.join($source_dir, 'Fastlane')
+		$current_working_dir = File.expand_path File.dirname(__FILE__)
     $source_template_dir = File.join($source_dir, 'XCFit_Templates')
 		$source_template_xcode7_dir = File.join($source_dir, 'Xcode7-Templates')
 	  $source_xcfit_dir = File.join($source_template_dir, 'XCFit')
@@ -30,14 +32,6 @@ module XCFit
         say XCFit::VERSION
       end
 
-			desc 'setup_xcfit_podfile', 'Setup XCFit Podfile template'
-			def setup_xcfit_podfile
-         puts '=======Creating Template Podfile for the XCFit project'
-				 puts '=======You need to replace targets with your project targets and comment unwanted targets ========'
-				 system("curl -s -O https://raw.githubusercontent.com/Shashikant86/XCFit/master/Cocoapods/Podfile")
-				 puts "Podfile successfully created in the current working directory here at #{Dir.getwd}/Podfile"
-			end
-
 			desc 'setup_xcode_templates', 'Generate All Xcode Templates for the Gherkin Feature Files & targets for Cucumberish and Fitnesse'
       def setup_xcode_templates
 				if File.exist?($root_xcfit_dir)
@@ -57,5 +51,21 @@ module XCFit
          puts 'You wont need to restart Xcode but do so if nesessary!'
          puts " ***************** Enjoy XCFit *****************"
       end
+
+			desc 'setup_xcfit_podfile', 'Setup XCFit Podfile template'
+			def setup_xcfit_podfile
+         puts '=======Creating Template Podfile for the XCFit project'
+				 puts '=======You need to replace targets with your project targets and comment unwanted targets ========'
+				 system("curl -s -O https://raw.githubusercontent.com/Shashikant86/XCFit/master/Cocoapods/Podfile")
+				 puts "Podfile successfully created in the current working directory here at #{Dir.getwd}/Podfile"
+			end
+
+			desc 'setup_xcfit_fastfile', 'Setup Fastlane Template'
+			def setup_xcfit_fastfile
+         puts '=======Creating Template Podfile for the XCFit project'
+				 puts '=======You need to replace schemes and other varibales as required ========'
+				 FileUtils.cp_r($source_fastlane_dir, FileUtils.pwd())
+				 puts '=======Fastlane with Fastfile setup successfully ========'
+			end
   end
 end
