f7c9786a4a6814c69d39b65959db86f287065d35
Shashikant86 <shashikant.jagtap@icloud.com>
2019-03-31 19:25:44 +0100

    Fixed xcodebuild errors

--- Sources/XCFitWaiter.swift
+++ Sources/XCFitWaiter.swift
@@ -12,28 +12,28 @@ import Foundation
 @available(OSX 10.11, *)
 extension XCFit {
 
-    open func whenIexpectElementToAppear(_ element: XCUIElement) {
+    open func whenIExpectElementToAppear(_ element: XCUIElement) {
         XCTContext.runActivity(named: "When I Expect Element \(element) to appear") { _ in
-            let result = waitForElementToAppearCommpleted(element)
+            let result = waitForElementToAppearCompleted(element)
             XCTAssertTrue(result)
         }
     }
 
-    open func whenIexpectElementTimeOut(_ element: XCUIElement) {
+    open func whenIExpectElementTimeOut(_ element: XCUIElement) {
         XCTContext.runActivity(named: "When I Expect Element \(element) to Timeout") { _ in
             let result = waitForElementToAppearTimedOut(element)
             XCTAssertTrue(result)
         }
     }
 
-    open func whenIexpectElementIncorrectOrder(_ element: XCUIElement) {
+    open func whenIExpectElementIncorrectOrder(_ element: XCUIElement) {
         XCTContext.runActivity(named: "When I Expect Element \(element) in incorrect order") { _ in
             let result = waitForElementToAppearIncorrectOrder(element)
             XCTAssertTrue(result)
         }
     }
 
-    open func whenIexpectElementInvertedFulfillment(_ element: XCUIElement) {
+    open func whenIExpectElementInvertedFulfillment(_ element: XCUIElement) {
         XCTContext.runActivity(named: "When I Expect Element \(element) in Inverted Fullfillment") { _ in
             let result = waitForElementToAppearinvertedFulfillment(element)
             XCTAssertTrue(result)
